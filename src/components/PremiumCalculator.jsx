74568a66ac1565135ed6ecd037d9d9450dbcb24b
hansama0902 <20091247@marist.edu>
2025-03-10 21:04:56 -0700

    Change Comments

--- src/components/PremiumCalculator.jsx
+++ src/components/PremiumCalculator.jsx
@@ -1,24 +1,22 @@
 const PremiumCalculator = ({ plan, userIncome, userAge }) => {
     let discount = 0;
     let basePremium = plan.base_premium;
-  
-    // 如果年龄超过 55，保费翻倍
+
     if (userAge > 35 && userAge <= 55) {
       basePremium *= 2;
     } else if (userAge > 55) {
       basePremium *= 3;
     }
   
-    // 计算折扣
     if (userIncome < 35000) {
-      discount = basePremium * 0.2; // 低收入用户 20% 折扣
+      discount = basePremium * 0.2; 
     } else if (userIncome < 50000) {
-      discount = basePremium * 0.1; // 中等收入用户 10% 折扣
+      discount = basePremium * 0.1; 
     }
   
     const finalPremium = basePremium - discount;
   
-    // 返回计算后的数据
+  
     return { basePremium, discount, finalPremium };
   };
   
